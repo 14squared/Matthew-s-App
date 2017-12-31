238d253fc794a9e07753a49fcf6bd42c69d8247e
Matthew Fegan <matthew@ifspace.org>
2017-12-30 22:26:02 -0500

    Initial push
    Most of this is just stuff that Android Studio created.

---
+++ App/app/src/main/java/com/example/matth/matthewsapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.matth.matthewsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void nextPage(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        Button button = (Button)view;
+        String message = button.getText().toString();
+        Log.d(TAG, message);
+        startActivity(intent);
+    }
+}
