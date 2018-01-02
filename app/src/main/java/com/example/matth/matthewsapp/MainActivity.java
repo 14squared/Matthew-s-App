1e6dcaea6b8da08afa3aa357ab6e90b75d1d1b72
Matthew Fegan <matthew@ifspace.org>
2018-01-02 12:55:52 -0500

    Changes

--- app/src/main/java/com/example/matth/matthewsapp/MainActivity.java
+++ app/src/main/java/com/example/matth/matthewsapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.matth.matthewsapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.annotation.NonNull;
@@ -9,13 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
     private static final String TAG = "MainActivity";
+    public static final String EXTRA_MESSAGE = "com.example.helloandroid.MESSAGE";
 
 
 
@@ -34,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Switching to next page");
         Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         v.vibrate(100);
+        Intent intent = new Intent(this, SecondActivity.class);
+        Button button = (Button)view;
+        String message = button.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        Log.d(TAG, message);
+        startActivity(intent);
 
     }
 
---
+++ app/src/main/java/com/example/matth/matthewsapp/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.matth.matthewsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String TAG = "SecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textField = (TextView)findViewById(R.id.selected_title);
+        textField.setText(message);
+        Log.d(TAG, message);
+    }
+}
